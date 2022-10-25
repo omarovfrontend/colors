0ffc3ff1cd8591ca9039d4584a5b80024dfaec22
Shamil Omarov <s_omarov05@mail.ru>
2022-10-25 23:51:28 +0300

    При закрытом замке, цвет не меняется

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const cols = document.querySelectorAll('.col');
 
 document.addEventListener('keydown', (event) => {
+  event.preventDefault();
   if (event.code.toLowerCase() === 'space') {
     setRandomColors();
+  }
+})
+
+document.addEventListener('click', (event) => {
+  const type = event.target.dataset.type;
+
+  if (type === 'lock') {
+    const node = event.target.tagName.toLowerCase() === 'i' ? event.target : event.target.children[0];
+
+    node.classList.toggle('fa-lock-open');
+    node.classList.toggle('fa-lock');
   }
 })
 
@@ -23,9 +35,14 @@ function generateRandomColor() {
 
 function setRandomColors() {
   cols.forEach((col) => {
+    const isLocked = col.querySelector('.fa-solid').classList.contains('fa-lock');
     const colTitle = col.querySelector('.col__title');
     const colBtn = col.querySelector('.col__btn');
     const color = generateRandomColor();
+
+    if (isLocked) {
+      return;
+    }
 
     colTitle.textContent = color;
     col.style.background = color;
